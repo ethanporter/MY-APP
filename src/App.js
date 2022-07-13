dc84121d1d9eed7bb79734dd20709c61a8eae19c
Ethan Porter <portere017@gmail.com>
2022-07-12 21:58:56 -0400

    seth test image not show

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import './App.css';
 import { Grid, Button } from '@mui/material';
 
 const App = () => {
+  let showpic = false;
+  function togglePic(){
+    console.log("iamhere"); 
+    showpic = !showpic; 
+    console.log(showpic); 
+  } 
   return (
     <Grid className="App">
       <header className="App-header">
-        <Button>enormous</Button>
+        {/* {showpic ? <img src={"unknown.png"} alt="hi"></img>: null} */}
+        {showpic ? (<p>hello</p>): null}
+        <Button onClick={togglePic}>enormous</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, createEvent, fireEvent } from '@testing-library/react';
 import App from './App';
 
+beforeEach(() => {render(<App />)}); 
+
 test('there should be a button on my page', () => {
-  render(<App />);
   expect(screen.getByText("enormous")).toBeInTheDocument();
   expect(screen.getAllByRole("button")).toHaveLength(1);
 });
+
+
+test('adding button function', ()=> {
+  const button = screen.getByText("enormous")
+  const buttClick = createEvent.click(button, {showpic: true})
+  fireEvent(button, buttClick)
+  expect(screen.getByAltText("hi")).toBeInTheDocument(); 
+}); 
